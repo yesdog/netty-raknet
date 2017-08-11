a4fff0aa6039556b875bc9ada5a75d0311170142
Shevchik <Shev4ik.den@gmail.com>
2017-08-11 17:02:40 +0300

    Remove unused method

--- src/raknetserver/utils/Utils.java
+++ src/raknetserver/utils/Utils.java
@@ -1,9 +1,5 @@
 package raknetserver.utils;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import io.netty.buffer.ByteBuf;
 
 public class Utils {
@@ -16,22 +12,6 @@ public class Utils {
 		byte[] data = new byte[length];
 		from.readBytes(data);
 		return data;
-	}
-
-
-	public static List<int[]> splitArray(int[] array, int limit) {
-		List<int[]> list = new ArrayList<int[]>();
-		if (array.length <= limit) {
-			list.add(array);
-			return list;
-		}
-		int count = getSplitCount(array.length, limit);
-		int copied = 0;
-		for (int i = 0; i < count; i++) {
-			list.add(Arrays.copyOfRange(array, copied, Math.min(array.length, copied + limit)));
-			copied += limit;
-		}
-		return list;
 	}
 
 	public static int getSplitCount(int length, int maxlength) {
